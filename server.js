f89452bcbb9bed8f13b0578a118f8eb643d50718
yojimat <visoccer22@hotmail.com>
2019-07-02 13:12:22 -0300

    Bugfix#3

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get("*", (req, res) => {
     );
 });
 
-app.listen(5000, () => {
-  console.log('Listening on port 5000!');
+app.listen(process.env.PORT || 5000, () => {
+	
+  console.log(`Listening on port${process.env.PORT || 5000}`);
 });
